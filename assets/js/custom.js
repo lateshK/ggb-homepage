707618a7d59e3987196606aa0d5439ec1ccedaa3
Ankita <ankita@ajency.in>
2020-06-10 14:43:22 +0530

    partial slide visible

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -72,16 +72,36 @@ if ($(window).width() < 1198) {
 }
 
 $(document).ready(function(){       
-    $('.product-image').slick({
-        dots: true,
-        infinite: true,
-        speed: 700,
-        autoplay:true,
-        autoplaySpeed: 2000,
-        arrows:false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-     });
+    var $carousel = $('.product-image');
+
+    var settings = {
+      dots: true,
+      arrows: false,
+      slide: '.slick-slideshow__slide',
+      slidesToShow: 1,
+      centerMode: true,
+      centerPadding: '60px',
+    };
+
+    function setSlideVisibility() {
+      //Find the visible slides i.e. where aria-hidden="false"
+      var visibleSlides = $carousel.find('.slick-slideshow__slide[aria-hidden="false"]');
+      //Make sure all of the visible slides have an opacity of 1
+      $(visibleSlides).each(function() {
+        $(this).css('opacity', 1);
+      });
+
+      //Set the opacity of the first and last partial slides.
+      $(visibleSlides).first().prev().css('opacity', 0);
+    }
+
+    $carousel.slick(settings);
+    $carousel.slick('slickGoTo', 1);
+    setSlideVisibility();
+
+    $carousel.on('afterChange', function() {
+      setSlideVisibility();
+    });
 });
 
 $('#showdiv1').click(function() {
